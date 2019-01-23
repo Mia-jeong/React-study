378d4b377c171db9f2c5c5ddb6233ae43681dc3d
Mia-jung <miajeongdev@gmail.com>
2019-01-23 21:25:02 +0900

    request data from outside Of API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,39 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+  async onSearchSubmit(term) {
+    //Request made to unsplash API
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID "
+      }
+    });
+    /* .then(response => {
+        //request complete
+        console.log(response.data.results);
+      }); */
+    this.setState({ images: response.data.results });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length} images
+      </div>
+    );
+  }
+}
+
+//when you pass in properties or callbacks to components that you and I we can call them anything that we want
+// so you can write onSubmit as Something you want like runSubmit
+
+//there are two library to request Network in React
+// 1. axio > 3rd party package   --it's better
+// 2. fetch > function built into modern browsers
 
 export default App;
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -2,6 +2,7 @@ import React from "react";
 
 class SearchBar extends React.Component {
   state = { term: "" };
+
   onInputChange(event) {
     console.log(event.target.value);
   }
@@ -9,10 +10,24 @@ class SearchBar extends React.Component {
   inInputClick() {
     console.log("input was clicked");
   }
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    //to get access to the callback that was passed from app down to our search bar we would reference this.props
+    this.props.onSubmit(this.state.term);
+  };
+
+  //onFormSubmit (event) {
+  //  event.preventDefault();
+
+  // console.log(this.state.term);
+  //};
+  //e) => this.onFormSubmit(e)
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label> Image Search </label>
             <input
@@ -26,8 +41,9 @@ class SearchBar extends React.Component {
     );
   }
 
-  // if i put this.onInputChange() with parenthesis, it's going to be called automatically when SearchBar is rendered, so if you want to call it after it's rendered, you should put this.onInpuchange
-  // if you just do one single like for onChange it's better for you to write it out like  <input type="text" onChange={e => console.log(e.target.value)} />
+  // if i put this.onInputChange() with parenthesis, it's going to be called automatically when SearchBar is rendered,
+  // so if you want to call it after it's rendered, you should put it without parenthesis like this.onInpuchange
+  // if you just put one single line for onChange it's better for you to write it out like  <input type="text" onChange={e => console.log(e.target.value)} />
 
   // FLOW
 
