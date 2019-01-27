bd424e2290d75e4c3205b0e56036c39aa8a9a2fd
Mia-jung <miajeongdev@gmail.com>
2019-01-27 18:12:27 +0900

    make new project using Youtube API

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID input your Key"
+    Authorization:
+      "Client-ID 85dbae96f150507f11cd1c1a86d7d5d4086d0bedfcc45578ab01af48c70e87ff"
   }
 });
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    // TO DO: Make sure we call
+    // callback from parent component
+    this.props.onFormSubmit(this.state.term);
+  };
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={this.state.term}
+            onChange={this.onInputChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
