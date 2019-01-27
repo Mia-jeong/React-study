8432abc1be5f9087d346fc75c288d73e4b5254ec
Mia <35101854+Mia-jeong@users.noreply.github.com>
2019-01-27 18:22:54 +0900

    Update unsplash.js

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID 85dbae96f150507f11cd1c1a86d7d5d4086d0bedfcc45578ab01af48c70e87ff"
+      "Client-ID input your key"
   }
 });
