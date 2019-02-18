e1a5d723f6bd6f3b2bd5a031c6c62ed683ddba77
Mia-jung <miajeongdev@gmail.com>
2019-02-18 14:23:29 +0900

    refactor code using StreamForm for StreamCreate and StreamEdit

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,97 +1,23 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-//Field : component which shows on the screen
-//reduxForm : function exactly the same as the connect function
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    //console.log(meta); //you can see the error message which error object have same prop as name of Field. error object is passed down to renderInput method
-    /*
-    return (
-      <input
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      />
-    );
-    */
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
-
   onSubmit = formValues => {
-    //event.preventDefault(); handleSubmit automatically take event value and  do it for us
-    //console.log("value", formValues);
     this.props.createStream(formValues);
   };
   render() {
-    //console.log(this.props);
     return (
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
 
-const validate = formValues => {
-  //console.log("validate", formValues); it's invoked whenever it's changed (onChange)
-  const error = {};
-  if (!formValues.title) {
-    // only ran if the user did not enter title
-    error.title = "You must enter a title";
-  }
-
-  if (!formValues.description) {
-    error.description = "You must eneter a description";
-  }
-  return error;
-};
-
-const formWrapped = reduxForm({
-  form: "streamCreate", //name of this form
-  validate
-})(StreamCreate);
-
 export default connect(
   null,
   { createStream }
-)(formWrapped);
-
-//to make new server for API(stores the list of detail of Streaming currently in server)
-/* 
-cd <folder>
-npm init
-npm install --save json-server
-
-
-code .  < command line to open code editor
-
-
-*/
+)(StreamCreate);
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,17 +1,36 @@
+import _ from "lodash";
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
+
+  onSubmit = formValues => {
+    console.log("edit formValue", formValues);
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
   render() {
-    console.log(this.props);
+    //console.log(this.props);
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    //first curly braces: we're using java script express
+    //seconde cruly braces : we're using object
+
+    //you should pass initailValues that only you want to change to reducForm
+    return (
+      <div>
+        <h3>Edit Steram</h3>
+        <StreamForm
+          initialValues={_.pick(this.props.stream, "title", "description")}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -20,5 +39,5 @@ const mapStateToProps = (state, ownProps) => {
 };
 export default connect(
   mapStateToProps,
-  { fetchStream }
+  { fetchStream, editStream }
 )(StreamEdit);
