ff87e3171b104e7b44005a77e6f79b9cbe7785aa
Mia-jung <miajeongdev@gmail.com>
2019-02-28 00:30:44 +0900

    using Hooks

---
+++ hooks-simple/src/components/App.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import ResourceList from "./ResourceList";
+
+const App = () => {
+  const [resource, setResource] = useState("posts");
+
+  return (
+    <div className="ui container">
+      <div>
+        <button
+          className="ui button primary"
+          onClick={() => setResource("posts")}
+        >
+          Posts
+        </button>
+        <button
+          className="ui button primary"
+          onClick={() => setResource("todos")}
+        >
+          Todos
+        </button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
+
+/* 
+Name                Goal
+useState            Allow a functional component to use component-level state
+useEffect           Allow a functional component to use 'lifecycle methods'
+useContext          Allow a functional component to use the context system
+useRef              Allow a functional component to use the ref System
+useReducer          Allow a functional component to store data through a 'reducer'
+
+
+*/
+export default App;
---
+++ hooks-simple/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+//to install hook
+//npm install --save react@next react-dom@next
+//to install axios
+////npm install --save axios
+ReactDOM.render(<App />, document.querySelector("#root"));
